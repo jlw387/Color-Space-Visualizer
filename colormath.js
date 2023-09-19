a94bc79963d23c3f43596e6aedc466c5f4794b81
John <jlw387@cornell.edu>
2023-09-19 16:52:44 -0400

    Updates

--- colormath.js
+++ colormath.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+import * as THREE from 'three';
 
 export function UpdateParallelipiped(piped, mat, hex)
 {
