62c5d501fa7648187e8562a122a25e47fc3b748b
John <jlw387@cornell.edu>
2023-09-19 16:16:54 -0400

    Updates

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 //import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 import * as colormath from './colormath.js';
